0f1ec1e307392fd24b402ca1b3b4ece073478ec1
Matias Agustin Lassalle <maticxen@gmail.com>
2025-08-31 19:53:36 -0300

    borre rama para empezar con el proyecto

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,9 +6,7 @@ public class GameManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Game Manager Started");
-        Debug.Log("New feature added in the branch");
-        Debug.Log("Another log message");
+      
     }
 
     // Update is called once per frame
